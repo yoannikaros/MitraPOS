38a4b1213055d64ff7814d14cc13f2ebd136b48f
Yoan Nikaros <yoannikaros29@gmail.com>
2024-11-15 14:58:53 +0700

    Initial Commit

---
+++ lib/view/HomePage/HomePage.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatelessWidget {
+  const Homepage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        children: [
+          Container(
+            child: Text('data'),
+          )
+        ],
+      ),
+    );
+  }
+}
