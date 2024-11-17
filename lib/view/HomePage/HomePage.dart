bc031fd96c9ad6d406000582c7bf96eaf1b314ad
Yoan Nikaros <yoannikaros29@gmail.com>
2024-11-17 17:54:45 +0700

    MENU KASIR

--- lib/view/HomePage/HomePage.dart
+++ lib/view/HomePage/HomePage.dart
@@ -15,33 +15,157 @@ class _HomepageState extends State<Homepage> {
         child: Row(
           children: [
             Text(
-              'Order',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+              'Toko',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 35),
             ),
             SizedBox(
               width: 8,
             ),
             Text(
-              'Menu',
-              style: TextStyle(fontWeight: FontWeight.normal, fontSize: 30),
+              'Lina',
+              style: TextStyle(fontWeight: FontWeight.normal, fontSize: 35),
             ),
             Spacer(),
             Container(
               height: 45,
               width: 300,
-              child: Center(
-                child: Row(
-                  children: [
-                    SizedBox(width: 20,),
-                    Icon(Icons.search,color: Colors.grey,),
-                    SizedBox(width: 20,),
-                    Text('Search for food, drink ',style: TextStyle(color: Colors.grey),),
-                  ],
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.white,
+              ),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Search for food, drink',
+                  hintStyle: TextStyle(color: Colors.grey), // Warna placeholder
+                  prefixIcon:
+                      Icon(Icons.search, color: Colors.grey), // Ikon pencarian
+                  border: InputBorder.none, // Hilangkan garis bawah default
+                  contentPadding: EdgeInsets.symmetric(
+                      vertical: 12, horizontal: 20), // Padding konten
                 ),
+                style: TextStyle(color: Colors.black), // Gaya teks yang diketik
+              ),
+            )
+          ],
+        ),
+      );
+    }
+
+    Widget Category() {
+      return Container(
+        margin: EdgeInsets.symmetric(vertical: 15),
+        child: Row(
+          children: [
+            Container(
+              height: 40,
+              padding: EdgeInsets.all(10),
+              child: Text(
+                'Drink',
+                style:
+                    TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
               ),
               decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10), color: Colors.white),
-            )
+                  color: Colors.redAccent,
+                  borderRadius: BorderRadius.circular(5)),
+            ),
+            SizedBox(width: 10,),
+            Container(
+              height: 40,
+              padding: EdgeInsets.all(10),
+              child: Text(
+                'Makanan dan Minuman',
+                style:
+                TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+              ),
+              decoration: BoxDecoration(
+                  color: Colors.redAccent,
+                  borderRadius: BorderRadius.circular(5)),
+            ),
+            SizedBox(width: 10,),
+            Container(
+              height: 40,
+              padding: EdgeInsets.all(10),
+              child: Text(
+                'Makanan dan Minuman',
+                style:
+                TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+              ),
+              decoration: BoxDecoration(
+                  color: Colors.redAccent,
+                  borderRadius: BorderRadius.circular(5)),
+            ),            SizedBox(width: 10,),
+            Container(
+              height: 40,
+              padding: EdgeInsets.all(10),
+              child: Text(
+                'Makanan dan Minuman',
+                style:
+                TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+              ),
+              decoration: BoxDecoration(
+                  color: Colors.redAccent,
+                  borderRadius: BorderRadius.circular(5)),
+            ),            SizedBox(width: 10,),
+            Container(
+              height: 40,
+              padding: EdgeInsets.all(10),
+              child: Text(
+                'Makanan dan Minuman',
+                style:
+                TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+              ),
+              decoration: BoxDecoration(
+                  color: Colors.redAccent,
+                  borderRadius: BorderRadius.circular(5)),
+            ),            SizedBox(width: 10,),
+            Container(
+              height: 40,
+              padding: EdgeInsets.all(10),
+              child: Text(
+                'Makanan dan Minuman',
+                style:
+                TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+              ),
+              decoration: BoxDecoration(
+                  color: Colors.redAccent,
+                  borderRadius: BorderRadius.circular(5)),
+            ),
+
+          ],
+        ),
+      );
+    }
+
+
+    Widget Content() {
+      return Container(
+        margin: EdgeInsets.symmetric(vertical: 10),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Container(
+              height: 200,
+              width: 150,
+              child: Column(
+                children: [
+                  Container(
+                    height: 120,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      color: Colors.redAccent,
+                      borderRadius: BorderRadius.circular(10)
+                    ),
+                  ),
+                  SizedBox(height: 12,),
+                  Text('Nasi Goreng ayam',style: TextStyle(fontWeight: FontWeight.bold),),
+                  SizedBox(height: 5,),
+                  Text('Rp. 5000')
+                ],
+              ),
+              decoration: BoxDecoration(
+                  color: Colors.white, borderRadius: BorderRadius.circular(12)),
+            ),
+
           ],
         ),
       );
@@ -64,9 +188,10 @@ class _HomepageState extends State<Homepage> {
                       margin:
                           EdgeInsets.symmetric(horizontal: 20, vertical: 40),
                       child: Column(
-                        children: [
-                          header(),
-                        ],
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [header(),SingleChildScrollView(
+                            scrollDirection: Axis.horizontal,
+                            child: Category()), Content()],
                       ),
                     ),
                   ),
