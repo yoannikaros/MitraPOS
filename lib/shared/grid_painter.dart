8343ef56254dd5fe768c148b4dadbfebbe07969d
Yoan Nikaros <75083157+yoannikaros@users.noreply.github.com>
2024-11-24 22:00:42 +0700

    Add Assets

---
+++ lib/shared/grid_painter.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class GridPainter extends CustomPainter {
+  final double gridSize;
+
+  GridPainter({required this.gridSize});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final Paint paint = Paint()
+      ..color = Colors.grey.withOpacity(0.5)
+      ..style = PaintingStyle.stroke;
+
+    for (double x = 0; x < size.width; x += gridSize) {
+      canvas.drawLine(Offset(x, 0), Offset(x, size.height), paint);
+    }
+
+    for (double y = 0; y < size.height; y += gridSize) {
+      canvas.drawLine(Offset(0, y), Offset(size.width, y), paint);
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return false;
+  }
+}
